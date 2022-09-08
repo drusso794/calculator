8e0224ffa4ebba11df22cd986f72ae328cbdb993
Dominic Russo <drusso794@gmail.com>
2022-09-07 22:36:21 -0500

    operator function defined

--- calc.js
+++ calc.js
@@ -14,9 +14,19 @@ function divide(a, b) {
     return a / b;
 };
 
+function operate(operator, ...operands) {
+    if (operator == '+') {
+        return add(...operands);
+    } else if (operator == '-') {
+        return subtract(...operands);
+    } else if (operator == '*') {
+        return multiply(...operands);
+    } else if (operator == '/') {
+        return divide(...operands);
+    };
+};
 
-
-
+console.log(operate('/', 2, 7));
 
 
 
